687f120b63ecebaee774ae81d3c6eb1da89af8ac
Rhysis <64549358+Rhysis@users.noreply.github.com>
2020-04-30 17:35:44 +0200

    Create edouard.js
    Atelier Epitch 1

---
+++ edouard.js
@@ -0,0 +1,13 @@
+// Test affichage
+let A = 42;
+console.log(A);
+
+// Premier exercice
+console.log(4+2);
+console.log("4 + 2");
+console.log("4"+"2");
+
+// On peut utiliser const pour définir une variable constante qui ne pourra pas être modifiée
+const a = 0;
+// a = 1; // Provoque une erreur car on essaie de changer la valeur d'une variable constante
+console.log(a);
